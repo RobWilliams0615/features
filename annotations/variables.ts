4747ee151c4bcdd617e0c17f02b693d85876e07a
Rob <ricowilliams@protonmail.com>
2022-04-30 07:51:38 -0600

    initial commit

---
+++ annotations/variables.ts
@@ -0,0 +1,31 @@
+let  apples: number = 9;
+
+apples = 7
+
+let speed: string = 'yellow'
+
+let hasName: boolean = true
+let nothingMuch: null = null
+let nothing: undefined = undefined
+
+//built in objects
+
+let now: Date= new Date()
+let colors: string[] = ['red', 'green']
+let myNumbers: number[] = [1,2,3]
+
+//classes
+
+class Car {
+
+}
+
+let car: Car = new Car()
+
+//object literal
+
+let point: { x: number; y: number} = {
+  x: 10,
+  y: 20
+}
+
